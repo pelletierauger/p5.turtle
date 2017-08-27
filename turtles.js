69c5a3ba67ca9716b85e4dd3c3293efe643aff8c
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-08-26 23:22:51 -0400

    Define a beautiful path for Irene

--- turtles.js
+++ turtles.js
@@ -1,27 +1,27 @@
-var bob = new Turtle(90, new p5.Vector(0, 0));
-bob.name = "Bob";
-bob.instructions = function() {
-    var s = 10;
-    repeat(3, function() {
-        repeat(30, function() {
-            forward(s);
-            right(20);
-            s += 0.1;
-        });
-        repeat(60, function() {
-            forward(s);
-            left(20);
-            s += 0.1;
-        });
-    });
-};
+// var bob = new Turtle(90, new p5.Vector(0, 0));
+// bob.name = "Bob";
+// bob.instructions = function() {
+//     var s = 10;
+//     repeat(3, function() {
+//         repeat(30, function() {
+//             forward(s);
+//             right(20);
+//             s += 0.1;
+//         });
+//         repeat(60, function() {
+//             forward(s);
+//             left(20);
+//             s += 0.1;
+//         });
+//     });
+// };
 
 var irene = new Turtle(0, new p5.Vector(10, 0));
 irene.name = "Irene";
 irene.instructions = function() {
-    var s = 10;
-    repeat(30, function() {
-        repeat(30, function() {
+    var s = 5;
+    repeat(4, function() {
+        repeat(60, function() {
             forward(s);
             right(20);
             s += 0.1;
@@ -29,7 +29,11 @@ irene.instructions = function() {
         repeat(60, function() {
             forward(s);
             left(20);
-            s += 0.1;
+            s -= 0.1;
         });
+        forward(90);
+        left(90);
+        forward(90);
     });
+    forward(s);
 };
