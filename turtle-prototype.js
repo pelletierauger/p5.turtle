299e288584e877341425f208cade8879c5f0d216
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-08-29 23:28:14 -0400

    Fix a bug with the awake boolean

--- turtle-prototype.js
+++ turtle-prototype.js
@@ -5,7 +5,7 @@ var Turtle = function(t) {
     this.heading = t.heading || 0;
     this.position = t.position || new p5.Vector(0, 0);
     this.penDown = true;
-    this.awake = (t.awake != null) ? t.awake : false;
+    this.awake = (t.awake != null) ? t.awake : true;
     if (this.awake) {
         t.instructions();
         this.states = instructionBox;
