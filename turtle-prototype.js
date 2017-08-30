971390f29e2273d6d21f7663701f6375fc78760e
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-08-29 22:20:02 -0400

    Add a way to make turtles sleep, create a new turtle path

--- turtle-prototype.js
+++ turtle-prototype.js
@@ -5,8 +5,11 @@ var Turtle = function(t) {
     this.heading = t.heading || 0;
     this.position = t.position || new p5.Vector(0, 0);
     this.penDown = true;
-    t.instructions();
-    this.states = instructionBox;
-    instructionBox = [];
-    turtles.push(this);
+    this.sleeping = t.sleeping || false;
+    if (!this.sleeping) {
+        t.instructions();
+        this.states = instructionBox;
+        instructionBox = [];
+        turtles.push(this);
+    }
 };
--- turtles.js
+++ turtles.js
@@ -1,102 +1,86 @@
-// var bob = new Turtle(90, new p5.Vector(0, 0));
-// bob.name = "Bob";
-// bob.instructions = function() {
-//     var s = 10;
-//     repeat(3, function() {
-//         repeat(30, function() {
-//             forward(s);
-//             right(20);
-//             s += 0.1;
-//         });
-//         repeat(60, function() {
-//             forward(s);
-//             left(20);
-//             s += 0.1;
-//         });
-//     });
-// };
+var irene = new Turtle({
+    sleeping: true,
+    heading: 0,
+    position: new p5.Vector(10, 0),
+    instructions: function() {
+        var s = 10;
+        repeat(3, function() {
+            repeat(30, function() {
+                forward(s);
+                right(20);
+                s += 0.1;
+            });
+            repeat(60, function() {
+                forward(s);
+                left(20);
+                s += 0.1;
+            });
+        });
+    }
+});
 
-// var irene = new Turtle(0, new p5.Vector(10, 0));
-// irene.name = "Irene";
-// irene.instructions = function() {
-//     var s = 5;
-//     repeat(4, function() {
-//         repeat(60, function() {
-//             forward(s);
-//             right(20);
-//             s += 0.1;
-//         });
-//         repeat(60, function() {
-//             forward(s);
-//             left(20);
-//             s -= 0.1;
-//         });
-//         forward(90);
-//         left(90);
-//         forward(90);
-//     });
-//     forward(s);
-// };
+var nina = new Turtle({
+    sleeping: true,
+    heading: 0,
+    position: new p5.Vector(0, 0),
+    instructions: function() {
+        var s = 5;
+        var times = 4;
+        repeat(times, function() {
+            repeat(60, function() {
+                forward(s);
+                right(20);
+                s += 0.1;
+            });
+            repeat(60, function() {
+                forward(s);
+                left(20);
+                s -= 0.1;
+            });
+            forward(60);
+            repeat(4, function() {
+                forward(s);
+                left(360 / times / 4);
+            });
+            forward(60);
+        });
+        forward(s);
+    }
+});
 
-// var nina = new Turtle({
-//     heading: 0,
-//     position: new p5.Vector(0, 0),
-//     instructions: function() {
-//         var s = 5;
-//         var times = 4;
-//         repeat(times, function() {
-//             repeat(60, function() {
-//                 forward(s);
-//                 right(20);
-//                 s += 0.1;
-//             });
-//             repeat(60, function() {
-//                 forward(s);
-//                 left(20);
-//                 s -= 0.1;
-//             });
-//             forward(60);
-//             repeat(4, function() {
-//                 forward(s);
-//                 left(360 / times / 4);
-//             });
-//             forward(60);
-//         });
-//         forward(s);
-//     }
-// });
-
-// var num = 10;
-// for (var i = 0; i < num; i++) {
-//     var a = 360 / num;
-//     var bob = new Turtle({
-//         heading: i * a,
-//         position: new p5.Vector(0, 0),
-//         instructions: function() {
-//             var j = 1;
-//             repeat(8, function() {
-//                 repeat(10, function() {
-//                     forward(5);
-//                     right(4);
-//                 });
-//                 if (j < 0) {
-//                     repeat(10, function() {
-//                         forward(5);
-//                         right(4);
-//                     });
-//                 } else {
-//                     repeat(10, function() {
-//                         forward(5);
-//                         left(4);
-//                     });
-//                 }
-//                 j *= -1;
-//             });
-//         }
-//     });
-// }
+var num = 10;
+for (var i = 0; i < num; i++) {
+    var a = 360 / num;
+    var bob = new Turtle({
+        sleeping: true,
+        heading: i * a,
+        position: new p5.Vector(0, 0),
+        instructions: function() {
+            var j = 1;
+            repeat(8, function() {
+                repeat(10, function() {
+                    forward(5);
+                    right(4);
+                });
+                if (j < 0) {
+                    repeat(10, function() {
+                        forward(5);
+                        right(4);
+                    });
+                } else {
+                    repeat(10, function() {
+                        forward(5);
+                        left(4);
+                    });
+                }
+                j *= -1;
+            });
+        }
+    });
+}
 
 var zyg = new Turtle({
+    sleeping: true,
     instructions: function() {
         repeat(6, function() {
             repeat(5, function() {
@@ -117,6 +101,22 @@ var zyg = new Turtle({
                 penDown();
             });
             left(20);
+        });
+    }
+});
+
+var cedric = new Turtle({
+    sleeping: true,
+    instructions: function() {
+        repeat(6, function() {
+            repeat(18, function() {
+                forward(3);
+                right(10);
+            });
+            repeat(36 + 12, function() {
+                forward(3);
+                left(10);
+            });
         });
     }
 });
